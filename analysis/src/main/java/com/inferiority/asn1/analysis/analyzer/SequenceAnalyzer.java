93e023f7a0324ea186af7868444979f7ace30717
JCUI21 <jcui21@ford.com>
2023-03-10 16:55:21 +0800

    .

--- analysis/src/main/java/com/inferiority/asn1/analysis/analyzer/SequenceAnalyzer.java
+++ analysis/src/main/java/com/inferiority/asn1/analysis/analyzer/SequenceAnalyzer.java
@@ -14,7 +14,8 @@ import com.inferiority.asn1.analysis.util.RegexUtil;
 public class SequenceAnalyzer extends AbstractAnalyzer {
     private static final SequenceAnalyzer analyzer = new SequenceAnalyzer();
 
-    public static final String REGEX_SEQUENCE_BODY = "(" + Operator.OPENING_BRACE + CRLF + "(" + REGEX_IDENTIFIER + CRLF_LEAST + "[\\S]+[,]?" + CRLF + ")+" +
+    public static final String REGEX_SEQUENCE_BODY = "(" + Operator.OPENING_BRACE + CRLF +
+            "(" + Operator.ELLIPSIS + "|" + REGEX_IDENTIFIER + CRLF_LEAST + "[\\S]+[,]?" + CRLF + ")+" +
             Operator.CLOSING_BRACE + ")";
 
     public static final String REGEX_SEQUENCE = CRLF + REGEX_IDENTIFIER + CRLF + Operator.ASSIGNMENT + CRLF + REGEX_IDENTIFIER + CRLF +
@@ -27,13 +28,11 @@ public class SequenceAnalyzer extends AbstractAnalyzer {
 
     @Override
     public Definition parse(String primitiveType, String text, String moduleText) throws AnalysisException {
-        //TODO 2023/3/9 18:20 REGEX_SEQUENCE 有可能是多层的定义
         if (!RegexUtil.matches(REGEX_SEQUENCE, text)) {
             throw new AnalysisException("not a valid sequence type definition.\n" + text);
         }
-        String primitive = Reserved.SEQUENCE + " " + Reserved.OF;
         Definition definition = new Definition();
-        definition.setPrimitiveType(primitive);
+        definition.setPrimitiveType(Reserved.SEQUENCE);
         definition.setDefinitionText(text);
         //identifier
         definition.setIdentifier(RegexUtil.matcher(REGEX_IDENTIFIER, text));
