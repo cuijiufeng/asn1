87800f89f31929c30bbc2ae651a1f037ab37afba
JCUI21 <jcui21@ford.com>
2023-03-22 12:49:53 +0800

    maven plugin junit

--- asn1-maven-plugin/src/test/java/com/inferiority/asn1/plugin/GenerateClassMojoTest.java
+++ asn1-maven-plugin/src/test/java/com/inferiority/asn1/plugin/GenerateClassMojoTest.java
@@ -1,31 +1,29 @@
 package com.inferiority.asn1.plugin;
 
+import org.apache.maven.execution.DefaultMavenExecutionRequest;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.ProjectBuilder;
+import org.apache.maven.project.ProjectBuildingRequest;
+import org.eclipse.aether.DefaultRepositorySystemSession;
 
 import java.io.File;
 
 public class GenerateClassMojoTest extends AbstractMojoTestCase {
 
-    protected void setUp() throws Exception {
-        // required for mojo lookups to work
-        super.setUp();
-    }
-
-    protected void tearDown() throws Exception {
-        // required
-        super.tearDown();
-    }
-
     /**
      * @throws Exception
      */
-    public void testMojoGoal() throws Exception {
+    public void testMojoGenerateGoal() throws Exception {
         File pom = getTestFile( "src/test/resources/project-to-test/pom.xml" );
         assertNotNull( pom );
         assertTrue( pom.exists() );
 
-        GenerateClassMojo mojo = (GenerateClassMojo) lookupMojo( "generate", pom );
-        assertNotNull( mojo );
+        ProjectBuildingRequest configuration = new DefaultMavenExecutionRequest()
+                .getProjectBuildingRequest()
+                .setRepositorySession(new DefaultRepositorySystemSession());
+        MavenProject project = lookup(ProjectBuilder.class).build(pom, configuration).getProject();
+        GenerateClassMojo mojo = (GenerateClassMojo) lookupConfiguredMojo(project, "generate");
         mojo.execute();
     }
 }
