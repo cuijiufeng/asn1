7b66937b5895ab3a5d89ebf2436228f1272ea892
cuijiufeng <1310639453@qq.com>
2023-03-25 00:00:18 +0800

    maven plugin

---
+++ asn1-maven-plugin/src/test/java/com/inferiority/asn1/plugin/GenerateClassMojoTest.java
@@ -0,0 +1,29 @@
+package com.inferiority.asn1.plugin;
+
+import org.apache.maven.execution.DefaultMavenExecutionRequest;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.ProjectBuilder;
+import org.apache.maven.project.ProjectBuildingRequest;
+import org.eclipse.aether.DefaultRepositorySystemSession;
+
+import java.io.File;
+
+public class GenerateClassMojoTest extends AbstractMojoTestCase {
+
+    /**
+     * @throws Exception
+     */
+    public void testMojoGenerateGoal() throws Exception {
+        File pom = getTestFile( "src/test/resources/project-to-test/pom.xml" );
+        assertNotNull( pom );
+        assertTrue( pom.exists() );
+
+        ProjectBuildingRequest configuration = new DefaultMavenExecutionRequest()
+                .getProjectBuildingRequest()
+                .setRepositorySession(new DefaultRepositorySystemSession());
+        MavenProject project = lookup(ProjectBuilder.class).build(pom, configuration).getProject();
+        GenerateClassMojo mojo = (GenerateClassMojo) lookupConfiguredMojo(project, "generate");
+        mojo.execute();
+    }
+}
---
+++ asn1-maven-plugin/src/test/resources/project-to-test/src/main/java/com/inferiority/protocol/Asn1Test.java
@@ -0,0 +1,14 @@
+package com.inferiority.protocol;
+
+import com.inferiority.protocol.sec.base.Uint3;
+
+/**
+ * @author cuijiufeng
+ * @Class Asn1Test
+ * @Date 2023/3/22 16:50
+ */
+public class Asn1Test {
+
+    public void testAsn1() {
+    }
+}
