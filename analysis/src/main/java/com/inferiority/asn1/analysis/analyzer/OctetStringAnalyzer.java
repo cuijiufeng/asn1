8519ae51705a48e250448669b62f52d501fba263
JCUI21 <jcui21@ford.com>
2023-03-29 09:04:44 +0800

    string bug

--- analysis/src/main/java/com/inferiority/asn1/analysis/analyzer/OctetStringAnalyzer.java
+++ analysis/src/main/java/com/inferiority/asn1/analysis/analyzer/OctetStringAnalyzer.java
@@ -13,9 +13,9 @@ import com.inferiority.asn1.analysis.util.RegexUtil;
 public class OctetStringAnalyzer extends AbstractStringAnalyzer {
     private static final OctetStringAnalyzer analyzer = new OctetStringAnalyzer();
 
-    public static final String REGEX_OCTET_STRING_RANGE = "(" + Operator.LEFT_BRACKET + Reserved.SIZE + Operator.LEFT_BRACKET +
-            REGEX_NUM_COMPOUND + "(" + Operator.RANGE + REGEX_NUM_COMPOUND + ")?" +
-            Operator.RIGHT_BRACKET + Operator.RIGHT_BRACKET + ")";
+    public static final String REGEX_OCTET_STRING_RANGE = "(" + Operator.LEFT_BRACKET + Reserved.SIZE + CRLF +
+            Operator.LEFT_BRACKET + REGEX_NUM_COMPOUND + "(" + Operator.RANGE + REGEX_NUM_COMPOUND + ")?" + Operator.RIGHT_BRACKET +
+            Operator.RIGHT_BRACKET + ")";
 
     public static final String REGEX_OCTET_STRING = REGEX_IDENTIFIER + CRLF + Operator.ASSIGNMENT + CRLF +
             "(" + REGEX_IDENTIFIER + "[ ]*)+" + CRLF + REGEX_OCTET_STRING_RANGE + "?" + CRLF;
